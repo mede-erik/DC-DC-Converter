fb812e4bc7dd5a5810c2c384222ea9538ea9733f
mede-erik <>
2025-02-15 20:00:30 +0100

    Fix merge conflicts and clean up code in ConverterPackage modules

--- ConverterPackage/boostlc.py
+++ ConverterPackage/boostlc.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 
 
@@ -8,19 +7,5 @@ def inductor_rating(Vin, duty_cycle, fsw, Irp):
 
 
 def capacitor_rating(output_current, duty_cycle, Vrp, fsw):
-    Co = np.divide(np.multiply(output_current, duty_cycle),
-                       np.multiply(Vrp, fsw))
-=======
-import numpy as np
-
-
-def inductor_rating(Vin, duty_cycle, fsw, Irp):
-    Lcr = np.divide(np.multiply(Vin, duty_cycle), np.multiply(fsw, Irp))
-    return Lcr
-
-
-def capacitor_rating(output_current, duty_cycle, Vrp, fsw):
-    Co = np.divide(np.multiply(output_current, duty_cycle),
-                       np.multiply(Vrp, fsw))
->>>>>>> 87820a8f68402c754ea9c9cdd6df2caab85774e4
+    Co = np.divide(np.multiply(output_current, duty_cycle), np.multiply(Vrp, fsw))
     return Co
